cdbe4a73d14b4bbd1f2c0a47653c7cd5b914b176
CryoTechnic <cryogen65@gmail.com>
2021-09-08 13:49:44 -0400

    Added 2nd view to Task1App + fixed structure

--- Task1Application/app/src/main/java/com/example/task1application/MainActivity.java
+++ Task1Application/app/src/main/java/com/example/task1application/MainActivity.java
@@ -2,13 +2,84 @@ package com.example.task1application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Creating instances of buttons and Textviews & var definition
+    Button childMinus, childPlus, adultMinus, adultPlus, scheduleButton;
+    TextView childCount, adultCount;
+    int adultTotal;
+    int childTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Assigning instanced button objects to buttons in application
+        childMinus = findViewById(R.id.minusChild);
+        childPlus = findViewById(R.id.plusChild);
+
+        adultMinus = findViewById(R.id.minusAdult);
+        adultPlus = findViewById(R.id.plusAdult);
+
+        childCount = findViewById(R.id.counterChild);
+        adultCount = findViewById(R.id.counterAdult);
+
+        scheduleButton = findViewById(R.id.scheduleButton);
+
+        // Event listeners for +/- buttons
+        childMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                childTotal--;
+                if (childTotal < 0) {
+                    childCount.setText("0");
+                }
+                childCount.setText(Integer.toString(childTotal));
+            }
+        });
+
+        childPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                childTotal++;
+                childCount.setText(Integer.toString(childTotal));
+            }
+        });
+
+        adultMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adultTotal--;
+                if (adultTotal < 0) {
+                    adultCount.setText("0");
+                }
+                adultCount.setText(Integer.toString(adultTotal));
+            }
+        });
+
+        adultPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                adultTotal++;
+                adultCount.setText(Integer.toString(adultTotal));
+            }
+        });
+
+        // Start 2nd activity to display counter for each.
+        // TODO: Implement TextViews in 2nd Activity
+        scheduleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(i);
+            }
+        });
     }
 }
